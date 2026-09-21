b3018da75904990c2342013a96df0b786871d821
ChristopherLozada2005 <ChristopherLozada2005@gmail.com>
2026-09-21 10:31:26 -0500

    setup-and-conventions

---
+++ nuestro-jardin/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Nuestro Jardin Amarillo</h1>
+      <p>Un pequeno jardin hecho con cuidado.</p>
+    </main>
+  )
+}
+
+export default App
